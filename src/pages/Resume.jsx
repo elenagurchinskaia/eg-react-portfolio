91b424f6cd3018f229080c4531467c1d3f1691e0
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-09-02 00:29:41 -0500

    styling for Resume.jsx

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,13 +1,40 @@
+import "../styles/Resume.css";
+
 function Resume() {
   return (
-    <div>
-      <h1>Resume</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-        voluptas, quia, voluptatem, quae ipsa voluptate doloribus tempora
-        voluptatum quidem quos dolorum. Quibusdam, quia. Quisquam, voluptates
-        voluptatum. Quisquam, voluptates voluptatum.
-      </p>
+    <div className="resume-container">
+      {/* <h1>Resume</h1> */}
+
+      <div className="resume-section">
+        <h5>Front-End:</h5>
+        <ul className="list-unstyled">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+        </ul>
+      </div>
+
+      <div className="resume-section">
+        <h5>Back-End:</h5>
+        <ul className="list-unstyled">
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>MongoDB</li>
+        </ul>
+      </div>
+
+      <div className="resume-section">
+        <p>
+          <a
+            href="https://drive.google.com/file/d/1pZA6_dcJcUrQUh7iSwNJI3CLwZJvaKqF/view"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Download Resume
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
