855f87504119106ecbd83b31734a54ac49d3b548
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-09-01 21:59:12 -0500

    portfolio styling

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,4 +1,5 @@
 import Project from "../components/Project";
+import "../styles/portfolio.css";
 import projectImg1 from "../assets/images/project-01_.png";
 import projectImg2 from "../assets/images/project-02_.png";
 import projectImg3 from "../assets/images/project-03.jpg";
@@ -8,38 +9,52 @@ import projectImg6 from "../assets/images/project-06.jpg";
 
 function Portfolio() {
   return (
-    <div>
-      <h1>Projects</h1>
-      <Project
-        name={"Upstream Travel"}
-        description={"This is my first project"}
-        image={projectImg1}
-      />
-      <Project
-        name={"The Monkey's Cup"}
-        description={"This is my second project"}
-        image={projectImg2}
-      />
-      <Project
-        name={"Project3"}
-        description={"This is my third project"}
-        image={projectImg3}
-      />
-      <Project
-        name={"Project4"}
-        description={"This is my fourth project"}
-        image={projectImg4}
-      />
-      <Project
-        name={"Project5"}
-        description={"This is my fifth project"}
-        image={projectImg5}
-      />
-      <Project
-        name={"Project6"}
-        description={"This is my sixth project"}
-        image={projectImg6}
-      />
+    <div className="portfolio-container">
+      {/* <h1>Projects</h1> */}
+      <div className="d-flex flex-wrap justify-content-between">
+        <div className="flex-grow-1 col-md-4">
+          <Project
+            name={"Upstream Travel"}
+            // description={"This is my first project"}
+            image={projectImg1}
+          />
+        </div>
+        <div className="flex-grow-1 col-md-4">
+          <Project
+            name={"The Monkey's Cup"}
+            // description={"This is my second project"}
+            image={projectImg2}
+          />
+        </div>
+        <div className="flex-grow-1 col-md-4">
+          <Project
+            name={"Project #03"}
+            // description={"This is my third project"}
+            image={projectImg3}
+          />
+        </div>
+        <div className="flex-grow-1 col-md-4">
+          <Project
+            name={"Project #04"}
+            // description={"This is my fourth project"}
+            image={projectImg4}
+          />
+        </div>
+        <div className="flex-grow-1 col-md-4">
+          <Project
+            name={"Project #05"}
+            // description={"This is my fifth project"}
+            image={projectImg5}
+          />
+        </div>
+        <div className="flex-grow-1 col-md-4">
+          <Project
+            name={"Project #06"}
+            // description={"This is my sixth project"}
+            image={projectImg6}
+          />
+        </div>
+      </div>
     </div>
   );
 }
