e73c726ff44c52e226496d06f3eeb8df9fae2b1d
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-10-05 22:39:22 -0500

    updated skills section

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,24 +3,44 @@ import "../styles/Resume.css";
 function Resume() {
   return (
     <div className="resume-container">
+      <h1>SKILLS</h1>
       {/* <h1>Resume</h1> */}
 
       <div className="resume-section">
         <h5>Front-End:</h5>
         <ul className="list-unstyled">
+          <li>React</li>
+          <li>JavaScript</li>
+          <li>jQuery</li>
           <li>HTML</li>
           <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React</li>
         </ul>
       </div>
 
       <div className="resume-section">
         <h5>Back-End:</h5>
         <ul className="list-unstyled">
-          <li>Node.js</li>
-          <li>Express.js</li>
+          <li>mySQL</li>
+          <li>Sequelizer</li>
           <li>MongoDB</li>
+          <li>GraphQL</li>
+        </ul>
+      </div>
+
+      <div className="resume-section">
+        <h5>Other:</h5>
+        <ul className="list-unstyled">
+          <li>JWT</li>
+          <li>Postman</li>
+        </ul>
+      </div>
+
+      <div className="resume-section">
+        <h5>Deployment:</h5>
+        <ul className="list-unstyled">
+          <li>Git</li>
+          <li>Heroku</li>
+          <li>Netlify</li>
         </ul>
       </div>
 
